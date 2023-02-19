b696de04d4187106ca9b229deac65a6a8398722e
hadimajor <hudsonmajor7@gmail.com>
2023-02-19 03:58:32 -0800

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Program to print a series of strings
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char z[100] = "with proper grammer, but the outcome is a piece of art,";
+
+	printf("%s\n", z);
+	return (0);
+}
