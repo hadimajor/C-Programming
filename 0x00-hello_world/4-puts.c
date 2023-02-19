77cd8c6b510c300db292595f398f006829865ddf
hadimajor <hudsonmajor7@gmail.com>
2023-02-19 03:09:55 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: program that prints a string
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	getchar();
+	puts("Programming is like building a multilingual puzzle,");
+	return (0);
+}
