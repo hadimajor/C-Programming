741bd9fbab3f69b1fe7afb9d3c7f4ed1fce7a8d9
hadimajor <hudsonmajor7@gmail.com>
2023-02-20 14:36:05 -0800

    Patience, persistence and perspiration

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description: Program that prints combination
+* of single-digit numbers.
+* 
+* Return: 0 (Success)
+*
+*/
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		putchar(x + '0');
+		if (x < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
