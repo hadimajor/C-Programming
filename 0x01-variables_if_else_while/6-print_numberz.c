c5986f7bd76aad8b26ee006fa21916ff48b6256d
hadimajor <hudsonmajor7@gmail.com>
2023-02-20 12:42:44 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description: Program that prints numbers of base 10 using putchar.
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int a;
+
+	a = '0';
+	while (a < 10)
+	{
+	putchar(a + '0');
+		a++;
+	}
+	putchar ('\n');
+	return (0);
+}
