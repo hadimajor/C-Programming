99b09966208321eae592f79086686975335238a5
hadimajor <hudsonmajor7@gmail.com>
2023-02-20 07:31:26 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description: Program that prints the alphabet
+* in lowercase, then in uppercase.
+*
+*
+* Return: 0 (Success)
+*
+*/
+
+int main(void)
+{
+	char c = 'a';
+/* print the alphabet in lowercase */
+
+
+	for (c <= 'z'; c != "qe"; c++)
+	{
+		putchar(c'\n');
+	}
+	return (0);
+}
