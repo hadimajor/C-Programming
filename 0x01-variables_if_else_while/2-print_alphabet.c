241d8ca4e35fdc9e2d7c367ba6cfe1cb50005b78
hadimajor <hudsonmajor7@gmail.com>
2023-02-20 06:06:59 -0800

    alphABET

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description: Program that prints the alphabet
+* in lowercase, then in uppercase.
+*
+*
+* Return: 0 (Success)
+*
+*/
+
+int main(void)
+{
+	char c = 'a';
+/* print the alphabet in lowercase */
+	while (c <= 'z')
+	putchar(c++);
+/* reset c to 'A' which is 65 in ASCII */
+	c = 'A';
+/* print the alphabet in uppercase */
+	while (c <= 'Z')
+	putchar(c++);
+	putchar('\n');
+	return (0);
+}
