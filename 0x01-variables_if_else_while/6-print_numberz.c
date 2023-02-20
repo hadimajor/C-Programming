6e07a2167a30885fa7ca096df316564051a12a9e
hadimajor <hudsonmajor7@gmail.com>
2023-02-20 14:19:36 -0800

    Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -13,14 +13,8 @@ int main(void)
 	int a;
 
 	for (a = 0; a < 10; a++)
-	{
 		putchar(a + '0');
-		if (a < 9)
-		{
-			putchar(',');
-			putchar(' ');
-		}
-	}
+		while (a < 9)
 	putchar('\n');
 
 	return (0);
