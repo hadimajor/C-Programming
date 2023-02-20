b0f55040dabdd3698c4d73182cc4a4416a2f02de
hadimajor <hudsonmajor7@gmail.com>
2023-02-20 06:12:20 -0800

    When I cant fall asleep I play the alphabet game

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,7 +4,7 @@
 * main - Entry point
 *
 * Description: Program that prints the alphabet
-* in lowercase, then in uppercase.
+* in lowercase.
 *
 *
 * Return: 0 (Success)
@@ -15,12 +15,8 @@ int main(void)
 {
 	char c = 'a';
 /* print the alphabet in lowercase */
+
 	while (c <= 'z')
-	putchar(c++);
-/* reset c to 'A' which is 65 in ASCII */
-	c = 'A';
-/* print the alphabet in uppercase */
-	while (c <= 'Z')
 	putchar(c++);
 	putchar('\n');
 	return (0);
