35fc023e353e5878147841f51886c27d61c671ba
hadimajor <hudsonmajor7@gmail.com>
2023-02-20 15:23:57 -0800

    UNIX is basically a simple operating system

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Program that prints a sting of exact characters.
+ *
+ * Return: Always 1 (Success)
+ */
+
+int main(void)
+{
+	fwrite (2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
