b6697420fd52a1cd51ade5c778219b718f9ccdf6
hadimajor <hudsonmajor7@gmail.com>
2023-02-21 13:34:56 -0800

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description : Print _putchar
+ *
+ * Return : Returns 0 (Success)
+ */
+
+int main(void)
+{
+	write(1, "_putchar\n", 9);
+		return (0);
+}
