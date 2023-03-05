8d787e39ebaef40770585f53d36d447e7b1d2b27
hadimajor <hudsonmajor7@gmail.com>
2023-03-05 02:04:32 -0800

    Do not hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -9,5 +9,18 @@
  */
 int main(void)
 {
+	int myrandom;
+	int count;
+	int total;
+
+	srand(time(NULL));
+	for (count = 0, total = 2772; total > 122; count++)
+	{
+		myrandom = (rand() % 125) + 1;
+		printf("%c", myrandom);
+		total -= myrandom;
+	}
+	printf("%c", total);
+
 	return (0);
 }
