c392af209dac07187b9bfa9244c0b786f4f8adc1
hadimajor <hudsonmajor7@gmail.com>
2023-03-21 15:41:58 -0700

    Fxn creates a new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,67 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * _strdup - Function returns pointer to newly allocated space in memory
+ *
+ * @str: string to copy
+ *
+ * Return: pointer to the duplicated string, NULL if @str is NULL
+ */
+char *_strdup(char *str)
+{
+	char *ar;
+	unsigned int i = 0;
+	unsigned int j = 0;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[i])
+		i++;
+	ar = malloc(sizeof(char) * (i + 1));
+		if (ar == NULL)
+			return (NULL);
+	while (str[j])
+	{
+		ar[j] = str[j];
+		j++;
+	}
+	ar[j] = 0;
+	return (ar);
+}
+/**
+ * new_dog - creates new dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ * Return: NULL if function fails
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new;
+	char *ncpy;
+	char *ocpy;
+
+	new = malloc(sizeof(dog_t));
+	if (new == NULL)
+		return (NULL);
+
+	ncpy = _strdup(name);
+	if (!ncpy && name)
+	{
+		free(new);
+		return (NULL);
+	}
+	ocpy = _strdup(owner);
+	if (!ocpy && owner)
+	{
+		free(ncpy);
+		free(new);
+		return (NULL);
+	}
+
+	new->name = ncpy;
+	new->age = age;
+	new->owner = ocpy;
+
+	return (new);
+}
