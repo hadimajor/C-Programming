09b197b37dfc6275769f40a4ebac1c98c39b07f3
hadimajor <hudsonmajor7@gmail.com>
2023-03-23 16:05:21 -0700

    Fxn prints string

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_string - Fxn prints string
+ *
+ * @separator: string to be printed between string
+ * @n: number of stringpassed to fxn
+ *
+ * Return: nothing
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+	char *og;
+
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+	{
+		og = (va_arg(ap, char*));
+		if (og != NULL)
+			printf("%s", og);
+		else
+			printf("(nil)");
+		if (separator != NULL && i < n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(ap);
+}
