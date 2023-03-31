16b5e710442fb18c37638ad6cf0e0203e4450e27
hadimajor <hudsonmajor7@gmail.com>
2023-03-31 00:57:53 -0700

    Fxn prints a string before the main function is executed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_before_main - prints message before execute main
+ *
+ * Return: void
+ */
+void print_before_main(void)
+{
+	printf("You're beat! and yet, you must llow,\n");
+	printf("I bore my house upon my back!\n");
+}
